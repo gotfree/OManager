97f382b71c8f1c8d07511768b6a845efaab33360
Rob Kano <deathbydissection@gmail.com>
2019-11-11 00:40:12 +0400

    some first widgets

--- order_manager.py
+++ order_manager.py
@@ -4,6 +4,13 @@ from tkinter import *
 # Create window object
 app = Tk()
 
+# Order
+order = IntVar()
+order_label = Label(app, text="Order_id", font=("bold", 14), pady=20)
+order_label.grid(row=0, column=0)
+order_display = Entry(app, state="readonly", textvariable=order)
+order_display.grid(row=0, column=1)
+
 app.title("Order Manager")
 app.geometry("700x350")
 
