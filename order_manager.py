5b47782a8954752bc597b83b182d855a53346fd8
Rob Kano <deathbydissection@gmail.com>
2019-11-11 00:51:37 +0400

    configure widget

--- order_manager.py
+++ order_manager.py
@@ -7,7 +7,7 @@ app = Tk()
 # Order
 order = IntVar()
 order_label = Label(app, text="Order_id", font=("bold", 14), pady=20)
-order_label.grid(row=0, column=0)
+order_label.grid(row=0, column=0, sticky=W)
 order_display = Entry(app, state="readonly", textvariable=order)
 order_display.grid(row=0, column=1)
 
