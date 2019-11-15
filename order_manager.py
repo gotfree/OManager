02e4f63a6bd764ab2b81239e4cced54be3c3910b
Rob Kano <deathbydissection@gmail.com>
2019-11-15 23:31:12 +0400

    continue setup grid --FH

--- order_manager.py
+++ order_manager.py
@@ -9,11 +9,7 @@ app.title("Order Manager")
 app.geometry("1024x768")
 app.columnconfigure((1, 3, 5), weight=2)
 app.columnconfigure((0, 2, 4), weight=1)
-app.rowconfigure(0, weight=2)
-app.rowconfigure(1, weight=2)
-app.rowconfigure(2, weight=2)
-app.rowconfigure(3, weight=1)
-app.rowconfigure(4, weight=2)
+
 
 # date setup
 tz = pd.timezone("Europe/Astrakhan")
@@ -77,40 +73,40 @@ customer_label.grid(row=1, column=4)
 customer_display = Entry(app, textvariable=customer)
 customer_display.grid(row=1, column=5)
 
-# sku field ROW 2
+# sku field ROW 2-3
 sku = IntVar()
 sku_label = Label(app, text="sku", bg="green", fg="white")
-sku_label.grid(row=2, column=0)
+sku_label.grid(row=2, column=0, sticky=W)
 sku_display = Entry(app, textvariable=sku)
-sku_display.grid(row=2, column=1)
+sku_display.grid(row=3, column=0)
 
 # item_name
 item_name = StringVar()
 item_name_label = Label(app, text="Item name", bg="green", fg="white")
-item_name_label.grid(row=2, column=2)
+item_name_label.grid(row=2, column=1, sticky=W)
 item_name_display = Entry(app, textvariable=item_name)
-item_name_display.grid(row=2, column=3)
+item_name_display.grid(row=3, column=1, columnspan=5, sticky="EW")
 
 # quantity
 quantity = IntVar()
 quantity_label = Label(app, text="quantity", bg="green", fg="white")
-quantity_label.grid(row=2, column=4)
+quantity_label.grid(row=2, column=6, sticky=W)
 quantity_display = Entry(app, textvariable=quantity)
-quantity_display.grid(row=2, column=5)
+quantity_display.grid(row=3, column=6)
 
 # price
 price = DoubleVar()
 price_label = Label(app, text="price", bg="green", fg="white")
-price_label.grid(row=2, column=6)
+price_label.grid(row=2, column=7, sticky=W)
 price_display = Entry(app, textvariable=price)
-price_display.grid(row=2, column=7)
+price_display.grid(row=3, column=7)
 
-# comment field ROW 3
+# comment field ROW 4
 comment = StringVar()
 comment_label = Label(app, text="Comment", bg="green", fg="white")
-comment_label.grid(row=3, column=0)
+comment_label.grid(row=4, column=0)
 comment_display = Entry(app, textvariable=comment)
-comment_display.grid(row=3, column=1)
+comment_display.grid(row=4, column=1, columnspan=6, sticky="EW")
 
 # is active
 is_active = BooleanVar()
@@ -122,7 +118,7 @@ is_active_check = Checkbutton(
     variable=is_active,
     command=check_box_repr
 )
-is_active_check.grid(row=3, column=2)
+is_active_check.grid(row=4, column=7)
 
 
 # start program
