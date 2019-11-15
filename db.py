6a0ff81a66481824b1467704795d121eaba5f05c
gotfree <deathbydissection@gmail.com>
2019-11-15 17:15:15 +0400

    pretty print data from scheme --FW

--- db.py
+++ db.py
@@ -4,9 +4,22 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
+
+def myprint(d):
+  for k, v in d.items():
+    if isinstance(v, dict):
+      myprint(v)
+    elif isinstance(v, list):
+        # make key repr for items lists
+        for i in v:
+            print(f"{i}")
+    else:
+      pp.pprint(f"{k}")
+
+
 with open("db_scheme.yml", "r") as stream:
     try:
         D = yaml.safe_load(stream)
-        pp.pprint(D)
+        myprint(D)
     except yaml.YAMLError as exc:
         print(exc)
