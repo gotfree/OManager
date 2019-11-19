0ea58745af157fb0af8a98ddc56b1cecccd73153
Rob Kano <deathbydissection@gmail.com>
2019-11-19 22:06:05 +0400

    beautify window grid --FH

--- order_manager.py
+++ order_manager.py
@@ -37,65 +37,67 @@ def clear_list():
 # order_id ROW 0
 order = IntVar()
 order_label = Label(app, text="Order_id")
-order_label.grid(row=0, column=0, pady=10)
-order_display = Entry(app, state="readonly", textvariable=order)
-order_display.grid(row=0, column=1, sticky=W)
+order_label.grid(row=0, column=0, sticky=W, pady=10, padx=10)
+order_display = Entry(app, state="readonly", textvariable=order, justify=CENTER)
+order_display.grid(row=0, column=1, sticky=W, padx=10)
 
 # created_date
 created_date = StringVar()
 created_date_label = Label(app, text="Created date")
-created_date_label.grid(row=0, column=2)
+created_date_label.grid(row=0, column=3, sticky=W)
 created_date_display = Entry(
     app,
     state="readonly",
     textvariable=created_date,
-    # justify=CENTER,
+    justify=CENTER,
     # width=12
 )
 created_date.set(formatted_now)
-created_date_display.grid(row=0, column=3)
+created_date_display.grid(row=0, column=4)
 
 # place for 'updated date' field
 
 # deadline date picker
-deadline_label = Label(app, text="Set deadline")
-deadline_label.grid(row=0, column=6)
+deadline_label = Label(app, text="Set deadline", width=10)
+deadline_label.grid(row=0, column=6, sticky=W)
 cal = DateEntry(
     app,
     # background='darkblue',
     foreground="white",
     borderwidth=1,
     year=current_year,
+    justify=CENTER,
+    width=10,
 )
-cal.grid(row=0, column=7)
+cal.grid(row=0, column=7, sticky=W)
 
 # supplier ROW 1-2
 supplier = StringVar()
 supplier_label = Label(app, text="Supplier")
-supplier_label.grid(row=1, column=0, pady=10, sticky=W)
+supplier_label.grid(row=1, column=0, pady=10, sticky=W, padx=10)
 supplier_display = Entry(app, textvariable=supplier)
-supplier_display.grid(row=2, column=0, columnspan=2, sticky="EW")
+supplier_display.grid(row=2, column=0, columnspan=2, sticky="EW", padx=10)
 
 # shipper
 shipper = StringVar()
 shipper_label = Label(app, text="Shipper")
-shipper_label.grid(row=1, column=2, sticky=W)
+shipper_label.grid(row=1, column=3, sticky=W)
 shipper_display = Entry(app, textvariable=shipper)
-shipper_display.grid(row=2, column=2, columnspan=2, sticky="EW")
+shipper_display.grid(row=2, column=3, columnspan=2, sticky="EW")
 
 # customer
 customer = StringVar()
 customer_label = Label(app, text="Customer")
-customer_label.grid(row=1, column=4, sticky=W)
+customer_label.grid(row=1, column=6, sticky=W, padx=(0, 10))
 customer_display = Entry(app, textvariable=customer)
-customer_display.grid(row=2, column=4, columnspan=4, sticky="EW")
+customer_display.grid(row=2, column=6, columnspan=2, sticky="EW", padx=(0, 10))
 
 # sku field ROW 3-4
 sku = IntVar()
 sku_label = Label(app, text="sku")
-sku_label.grid(row=3, column=0, sticky=W, pady=5)
+sku_label.grid(row=3, column=0, sticky=W, pady=5, padx=(10, 0))
 sku_display = Entry(app, textvariable=sku)
-sku_display.grid(row=4, column=0, sticky="EW")
+sku_display.grid(row=4, column=0, sticky="EW", padx=(10, 0))
 
 # item_name
 item_name = StringVar()
@@ -114,9 +116,9 @@ quantity_display.grid(row=4, column=6)
 # price
 price = DoubleVar()
 price_label = Label(app, text="price")
-price_label.grid(row=3, column=7, sticky=W)
+price_label.grid(row=3, column=7, sticky=W, padx=(0, 10))
 price_display = Entry(app, textvariable=price)
-price_display.grid(row=4, column=7)
+price_display.grid(row=4, column=7, padx=(0, 10))
 
 # comment field ROW 5
 comment = StringVar()
@@ -139,28 +141,45 @@ is_active_check = Checkbutton(
 is_active_check.grid(row=5, column=7)
 
 # Order list ROW 6
-order_list = Listbox(app, height=20, width=125, border=0)
+order_list = Listbox(app, height=20, width=125)
 order_list.grid(row=6, column=0, columnspan=8)
 # scrollbar
 scrollbar = Scrollbar(app)
-scrollbar.grid(row=6, column=8)
+scrollbar.grid(row=6, column=7, sticky=E)
 # set scroll to listbox
 order_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=order_list.yview)
 
 # Buttons ROW 7
-add_btn = Button(app, text="Add Order", width=15, command=add_item)
+add_btn = Button(app, text="Add Order", width=20, command=add_item)
 add_btn.grid(row=7, column=2, pady=20)
 
-remove_btn = Button(app, text="Remove Order", width=15, command=remove_order)
+remove_btn = Button(app, text="Remove Order", width=20, command=remove_order)
 remove_btn.grid(row=7, column=3)
 
-update_btn = Button(app, text="Update Order", width=15, command=update_order)
+update_btn = Button(app, text="Update Order", width=20, command=update_order)
 update_btn.grid(row=7, column=4)
 
-clear_btn = Button(app, text="Clear input", width=15, command=clear_list)
+clear_btn = Button(app, text="Clear input", width=20, command=clear_list)
 clear_btn.grid(row=7, column=5)
 
+# ROW 8
+created_date_label = Label(app, text="test", bg="white")
+created_date_label.grid(row=8, column=0, sticky=NSEW)
+created_date_label = Label(app, text="test", bg="green")
+created_date_label.grid(row=8, column=1, sticky=NSEW)
+created_date_label = Label(app, text="test", bg="white")
+created_date_label.grid(row=8, column=2, sticky=NSEW)
+created_date_label = Label(app, text="test", bg="green")
+created_date_label.grid(row=8, column=3, sticky=NSEW)
+created_date_label = Label(app, text="test", bg="white")
+created_date_label.grid(row=8, column=4, sticky=NSEW)
+created_date_label = Label(app, text="test", bg="green")
+created_date_label.grid(row=8, column=5, sticky=NSEW)
+created_date_label = Label(app, text="test", bg="white")
+created_date_label.grid(row=8, column=6, sticky=NSEW)
+created_date_label = Label(app, text="test", bg="green")
+created_date_label.grid(row=8, column=7, sticky=NSEW)
 
 # start program
 app.mainloop()
